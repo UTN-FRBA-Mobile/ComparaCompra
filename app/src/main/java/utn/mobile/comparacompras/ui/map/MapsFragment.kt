77c0153d7953b44f7a656279736a1592be093645
franchupedu <franchupedu@outloook.com>
2022-07-06 19:38:55 -0300

    Primer intento del campo que autocompleta las addresses

--- app/src/main/java/utn/mobile/comparacompras/ui/map/MapsFragment.kt
+++ app/src/main/java/utn/mobile/comparacompras/ui/map/MapsFragment.kt
@@ -1,19 +1,34 @@
 package utn.mobile.comparacompras.ui.map
 
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.room.vo.Fields
+import com.android.volley.Response
+import com.google.android.gms.location.places.Place
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.LocationBias
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import utn.mobile.comparacompras.R
 import utn.mobile.comparacompras.databinding.FragmentMapsBinding
-
+import java.util.*
+import java.util.Arrays.asList
 
 class MapsFragment : Fragment() {
 
@@ -41,6 +56,23 @@ class MapsFragment : Fragment() {
         _binding = FragmentMapsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        var rectangularBounds: LocationBias? = null
+
+        val searchBar: EditText = binding.textSearch
+        Places.initialize(requireContext(), "AIzaSyDzPZqoCT9VURs5p7s-9YxFNWyUkIl4390");
+        var placesClient = Places.createClient(requireContext());
+        var request = FindAutocompletePredictionsRequest.builder().setLocationBias(rectangularBounds).setQuery(searchBar.text.toString()).build();
+        placesClient.findAutocompletePredictions(request).addOnSuccessListener({ response: Response<Places> ->
+            for (prediction in response.getAutocompletePredictions()) {
+            Log.i(TAG, prediction.getPlaceId())
+            Log.i(TAG, prediction.getPrimaryText(null).toString())
+        }
+        placesClient.findAutocompletePredictions(request).addOnSuccessListener((response) -> {
+            for (AutocompletePrediction prediction : response.getAutocompletePredictions()) {
+                Log.i(TAG, prediction.getPlaceId());
+                Log.i(TAG, prediction.getPrimaryText(null).toString());
+            }
+        })
         // Caso default: pongo la posicion de Sydney
         userPosition = LatLng(-34.0, 151.0)
 
